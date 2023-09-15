6ff38056e87567e5740fc70a203f73aad6b3760b
kajela <titoyut56@gmail.com>
2023-09-15 11:35:47 +0300

    add the initial files

---
+++ src/test/java/tito/ipl/Ipl/IplApplicationTests.java
@@ -0,0 +1,13 @@
+package tito.ipl.Ipl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
